dfe90993b05b4a80b877796f56ef6f78c0eac819
Aesir77 <134356658+Aesir77@users.noreply.github.com>
2025-10-29 01:07:33 +0800

    feat:Settings-Kim

---
+++ Assets/Scripts/Player Scripts/PlayerKeybinds.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerKeybinds : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsMenu;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            settingsMenu.SetActive(!settingsMenu.activeSelf);
+            Time.timeScale = settingsMenu.activeSelf ? 0f : 1f;
+        }
+    }
+}
